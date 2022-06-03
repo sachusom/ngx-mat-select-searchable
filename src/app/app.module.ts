28ccdc70fcdd61115f1568540168ccedb3ac10dd
Vishnu Somanath <Vishnu.Somanath@gbg.com>
2022-06-03 13:34:28 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { NgxMatSelectSearchableModule } from 'projects/ngx-mat-select-searchable/src/public-api';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMatSelectSearchableModule } from '@ngvslabs/ngx-mat-select-searchable';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
